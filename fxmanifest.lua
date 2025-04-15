ec65eef2536ffc2c35a367ca09b674e21e2a49d7
Greve <frankgrevekrogh@gmail.com>
2025-04-15 11:55:51 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,19 +1,17 @@
 fx_version 'cerulean'
 game 'gta5'
 
-description 'Core Resource - Mancave Roleplay'
+description 'Egghunt Resource - Interactive Resources'
 
-shared_scripts {
-    -- Add shared scripts here if needed
-}
+-- shared_scripts {
+--     -- Add shared scripts here if needed
+-- }
 
 client_scripts {
-    'modules/client/api-client/cl_mongodb.lua', -- MongoDB client API
     'modules/client/cl_egghunt.lua' -- Egg hunt client script
 }
 
 server_scripts {
-    'modules/server/api-server/sv_mongodb.lua', -- MongoDB server API
     'modules/server/sv_egghunt.lua' -- Egg hunt server script
 }
 
